30179c4df9c38c1721b003b3bac633c323068904
Inderpal Singh <inderpal.singh@lnds.lu>
2024-02-05 17:59:53 +0100

    Fix REUSE compliance

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2024 Stichting Health-RI
+// SPDX-FileContributor: PNED G.I.E.
+//
+// SPDX-License-Identifier: Apache-2.0
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
 

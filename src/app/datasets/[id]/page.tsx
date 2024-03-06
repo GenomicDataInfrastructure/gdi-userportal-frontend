cbf3882428397ff59fa161a434c28aa561626f47
Inderpal Singh <inderpal.singh@lnds.lu>
2024-03-06 11:56:11 +0100

    show it next to heading

--- src/app/datasets/[id]/page.tsx
+++ src/app/datasets/[id]/page.tsx
@@ -20,7 +20,10 @@ export default async function Page({ params }: { params: { id: string } }) {
     return (
       <div className="flex w-full flex-col items-start p-10 lg:flex-row">
         <div className="flex w-full flex-col gap-5 p-5 lg:w-2/3">
-          <PageHeading>{dataset.title}</PageHeading>
+          <div className="flex w-full flex-col items-start justify-between lg:flex-row lg:items-center">
+            <PageHeading className="w-2/3 sm:mb-4">{dataset.title}</PageHeading>
+            <AddToBasketBtn dataset={dataset} />
+          </div>
           <p>{dataset.notes}</p>
           {dataset.theme?.length > 0 && <PageSubHeading>Themes</PageSubHeading>}
           <Chips chips={dataset.theme || []} />
@@ -36,7 +39,6 @@ export default async function Page({ params }: { params: { id: string } }) {
             <PageSubHeading>Distributions</PageSubHeading>
           )}
           <DistributionAccordion distributions={dataset.distributions || []} />
-          <AddToBasketBtn dataset={dataset} />
         </div>
         <Sidebar dataset={dataset} />
       </div>

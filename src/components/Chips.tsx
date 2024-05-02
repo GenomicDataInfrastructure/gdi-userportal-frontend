ee3ef03fb2fbeece62cc696302908b9018e21739
Inderpal Singh <inderpal.singh@lnds.lu>
2024-05-02 12:22:35 +0200

    fix: pass down classes

--- src/components/Chips.tsx
+++ src/components/Chips.tsx
@@ -3,7 +3,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
 "use client";
-import { cn } from "@/utils/tailwindMerge";
 import Chip from "./Chip";
 import React from "react";
 
@@ -15,7 +14,7 @@ interface ChipsProps {
 const Chips: React.FC<ChipsProps> = ({ chips, className }) => (
   <div className={`flex flex-row flex-wrap gap-3 break-all`}>
     {chips.map((chip, index) => (
-      <Chip key={index} chip={chip} />
+      <Chip key={index} chip={chip} className={className} />
     ))}
   </div>
 );

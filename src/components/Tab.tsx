2fb1b124064c47345669fa11399d3e46722bb63f
Younès Adem <younes.adem@lnds.lu>
2024-05-07 16:14:49 +0200

    fix: add missing license

--- src/components/Tab.tsx
+++ src/components/Tab.tsx
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2024 PNED G.I.E.
+//
+// SPDX-License-Identifier: Apache-2.0
+
 import { IconDefinition } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import List from "./List";

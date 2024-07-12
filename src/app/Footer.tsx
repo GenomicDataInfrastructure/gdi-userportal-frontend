a75422e83262d5dbc5cbd8f8760a0352c8c1cd21
zia alborzi <zia.alborzi@lnds.lu>
2024-07-12 14:08:02 +0200

    organizing links in a tabular view

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -54,99 +54,119 @@ function Footer() {
           </div>
         </a>
       )}
-      <footer className="flex flex-col items-center justify-between gap-y-4 border-t-4 border-t-primary bg-surface p-7 md:flex-row md:gap-x-4 md:gap-y-0">
-        <div className="flex items-center gap-4">
-          <Image src={footerLogo} alt="Footer logo" width={80} />
-          <p className="text-xs md:text-sm">
-            {footerText.split("\n").map((line, index) => (
-              <span key={index}>
-                {line}
-                {index < footerText.split("\n").length - 1 && <br />}
-              </span>
-            ))}
-          </p>
-        </div>
-
-        <div className="flex flex-col gap-2">
-          <div className="flex flex-row justify-center gap-11 text-primary">
-            {linkedInUrl && (
+      <footer className="flex flex-col items-center justify-between gap-y-4 border-t-4 border-t-primary bg-surface p-7 md:flex-row md:items-start md:gap-x-12 md:gap-y-0">
+        <div className="flex flex-col gap-16 md:flex-row md:gap-24">
+          {/* First column: About the project */}
+          <div className="flex flex-col items-start gap-4">
+            <Image src={footerLogo} alt="Footer logo" width={80} />
+            <p className="text-xs md:text-sm">
+              {footerText.split("\n").map((line, index) => (
+                <span key={index}>
+                  {line}
+                  {index < footerText.split("\n").length - 1 && <br />}
+                </span>
+              ))}
+            </p>
+          </div>
+          {/* Second column: Fixed links */}
+          <div className="flex flex-col gap-2 text-left">
+            <h3 className="text-lg font-bold">Legal</h3>
+            <a className="hover:text-info">Terms & Conditions</a>
+            <a className="hover:text-info">Privacy Notice</a>
+            <a className="hover:text-info">Cookie Policy</a>
+          </div>
+          {/* Third column: Portal links */}
+          <div className="flex flex-col gap-2 text-left">
+            <h3 className="text-lg font-bold">Portal Links</h3>
+            <a href="/datasets" className="hover:text-info">
+              Datasets
+            </a>
+            <a className="hover:text-info">Themes</a>
+            <a className="hover:text-info">Organizations</a>
+          </div>
+          {/* Fourth column: Contact us */}
+          <div className="flex flex-col gap-2 text-left">
+            <h3 className="text-lg font-bold">Contact Us</h3>
+            <div className="flex gap-4">
+              {linkedInUrl && (
+                <a
+                  href={linkedInUrl}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="hover:text-info"
+                >
+                  <FontAwesomeIcon
+                    icon={faLinkedin}
+                    className="text-lg md:text-2xl"
+                  />
+                </a>
+              )}
+              {twitterUrl && (
+                <a
+                  color="primary"
+                  href={twitterUrl}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="hover:text-info"
+                >
+                  <FontAwesomeIcon
+                    icon={faXTwitter}
+                    className="text-lg md:text-2xl"
+                  />
+                </a>
+              )}
+              {githubUrl && (
+                <a
+                  color="primary"
+                  href={githubUrl}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="hover:text-info"
+                >
+                  <FontAwesomeIcon
+                    icon={faGithub}
+                    className="text-lg md:text-2xl"
+                  />
+                </a>
+              )}
+              {gitlabUrl && (
+                <a
+                  color="primary"
+                  href={gitlabUrl}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="hover:text-info"
+                >
+                  <FontAwesomeIcon
+                    icon={faGitlab}
+                    className="text-lg md:text-2xl"
+                  />
+                </a>
+              )}
+              {websiteUrl && (
+                <a
+                  color="primary"
+                  href={websiteUrl}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="hover:text-info"
+                >
+                  <FontAwesomeIcon
+                    icon={faGlobe}
+                    className="text-lg md:text-2xl"
+                  />
+                </a>
+              )}
+            </div>
+            {email && (
               <a
-                href={linkedInUrl}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="hover:text-info"
+                className="text-xs hover:text-info md:text-left md:text-sm"
+                href={`mailto:${encodeURIComponent(email)}`}
               >
-                <FontAwesomeIcon
-                  icon={faLinkedin}
-                  className="text-lg md:text-2xl"
-                />
-              </a>
-            )}
-            {twitterUrl && (
-              <a
-                color="primary"
-                href={twitterUrl}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="hover:text-info"
-              >
-                <FontAwesomeIcon
-                  icon={faXTwitter}
-                  className="text-lg md:text-2xl"
-                />
-              </a>
-            )}
-            {githubUrl && (
-              <a
-                color="primary"
-                href={githubUrl}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="hover:text-info"
-              >
-                <FontAwesomeIcon
-                  icon={faGithub}
-                  className="text-lg md:text-2xl"
-                />
-              </a>
-            )}
-            {gitlabUrl && (
-              <a
-                color="primary"
-                href={gitlabUrl}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="hover:text-info"
-              >
-                <FontAwesomeIcon
-                  icon={faGitlab}
-                  className="text-lg md:text-2xl"
-                />
-              </a>
-            )}
-            {websiteUrl && (
-              <a
-                color="primary"
-                href={websiteUrl}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="hover:text-info"
-              >
-                <FontAwesomeIcon
-                  icon={faGlobe}
-                  className="text-lg md:text-2xl"
-                />
+                {email}
               </a>
             )}
           </div>
-          {email && (
-            <a
-              className="text-xs hover:text-info md:text-left md:text-sm"
-              href={`mailto:${encodeURIComponent(email)}`}
-            >
-              {email}
-            </a>
-          )}
         </div>
       </footer>
     </>

ba8601fc7f47be79cc228940bec3fcca0992e245
Kacem Bechka <kacem198@yahoo.fr>
2024-07-15 09:10:05 +0200

    implementing Bruno's comment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,13 @@
 
 import PageContainer from "@/components/PageContainer";
 import SearchBar from "@/components/Searchbar";
-import Link from "next/link";
 import { useSearchParams } from "next/navigation";
 
 const HomePage = () => {
   const queryParams = useSearchParams();
-  const applicationTitle = "WELCOME TO GDI";
+  const applicationTitle = process.env.NEXT_PUBLIC_APPLICATION_TITLE || "WELCOME TO GDI";
   const applicationSubtitle =
+    process.env.NEXT_PUBLIC_APPLICATION_SUBTITLE ||
     "The Genomic Data Infrastructure (GDI) project is enabling access to genomic and related phenotypic and clinical data across Europe.";
 
   return (
@@ -21,18 +21,10 @@ const HomePage = () => {
         <h2 className="text-xl mt-4 font-light">{applicationSubtitle}</h2>
       </div>
       <div className="flex justify-center mb-8">
-        <div className="w-4/5">
+        <div className="w-3/5">
           <SearchBar queryParams={queryParams} size="large" />
         </div>
       </div>
-      <div className="flex justify-center mb-6">
-        <Link href="/datasets">
-          <button className="inline-block rounded-lg bg-primary px-4 py-2 font-bold text-white transition-colors duration-300 hover:bg-secondary">
-            Discover GDI datasets
-          </button>
-        </Link>
-      </div>
-
       <div className="mb-20">
         <div className="rounded-lg bg-white p-8 shadow-md transition-shadow duration-300 ease-in-out hover:shadow-lg">
           <h3 className="mb-4 text-2xl font-bold text-primary">Themes</h3>

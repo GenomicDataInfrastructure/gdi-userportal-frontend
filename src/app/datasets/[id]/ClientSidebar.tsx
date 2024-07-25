20de67a536b50368a2a655a625e23854862150f5
zia alborzi <zia.alborzi@lnds.lu>
2024-07-25 10:21:38 +0200

    dataset sidebar view

--- src/app/datasets/[id]/ClientSidebar.tsx
+++ src/app/datasets/[id]/ClientSidebar.tsx
@@ -14,9 +14,21 @@ type ClientSidebarProps = {
 
 function ClientSidebar({ dataset }: ClientSidebarProps) {
   const sidebarItems = createDatasetSidebarItems(dataset);
+  console.log('Dataset properties:', dataset);
   return (
-    <aside className="w-full lg:w-1/3">
-      <Sidebar items={sidebarItems} />
+    <aside className="w-full lg:w-1/3 lg:sticky top-24">
+      <div className="flex flex-col gap-4 pb-8">
+        {sidebarItems.map((item, index) => (
+          <div key={index} className="mb-3">
+            <h3 className="text-base text-primary sm:text-lg lg:text-xl">
+              {item.label}
+            </h3>
+            <span className="text-sm sm:text-base lg:text-lg">
+              {item.value}
+            </span>
+          </div>
+        ))}
+      </div>
     </aside>
   );
 }
--- src/app/datasets/[id]/sidebarItems.tsx
+++ src/app/datasets/[id]/sidebarItems.tsx
@@ -2,92 +2,167 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-import {
-  SidebarItem,
-  createTextItem,
-  createLinkItem,
-  createLinkItems,
-} from "@/components/Sidebar";
+import { SidebarItem } from "@/components/Sidebar";
 import { RetrievedDataset } from "@/services/discovery/types/dataset.types";
+import Link from "next/link";
 import { formatDate } from "@/utils/formatDate";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faUser } from "@fortawesome/free-solid-svg-icons";
 
 function createDatasetSidebarItems(dataset: RetrievedDataset): SidebarItem[] {
+  const metaFormats = [
+    {
+      format: "rdf",
+      label: "RDF",
+      style: {
+        backgroundColor: "var(--color-warning)",
+        ":hover": { backgroundColor: "var(--color-hover)" },
+      },
+    },
+    {
+      format: "ttl",
+      label: "TTL",
+      style: { backgroundColor: "var(--color-info)" },
+    },
+    {
+      format: "jsonld",
+      label: "JSON-LD",
+      style: { backgroundColor: "var(--color-secondary)", color: "white" },
+    },
+  ];
+
   return [
     {
-      label: "Metadata Created",
-      value: dataset.createdAt && (
-        <span className="font-date">{formatDate(dataset.createdAt)}</span>
+      value: (
+        <div
+          style={{ backgroundColor: "var(--color-surface)" }}
+          className="flex flex-col rounded-2xl p-6 gap-3"
+        >
+          <h1 className="font-bold">Request data access</h1>
+          <Link
+            href="https://lnds.lu/contact-us/"
+            className="flex justify-center items-center py-2 px-3.5 rounded-xl text-white hover:bg-[var(--color-hover)] w-fit transition ease-in-out duration-1000 text-[16px]medium"
+            style={{ backgroundColor: "var(--color-primary)" }}
+          >
+            Add to basket
+          </Link>
+        </div>
       ),
     },
     {
-      label: "Metadata Modified",
-      value: dataset.modifiedAt && (
-        <span className="font-date">{formatDate(dataset.modifiedAt)}</span>
+      value: (
+        <div
+          style={{ backgroundColor: "var(--color-surface)" }}
+          className="flex flex-col rounded-2xl p-6 gap-3"
+        >
+          <h1 className="font-bold  ">Export Metadata in</h1>
+          <div className="flex gap-2 transition py-2 sm:py-0">
+            {metaFormats.map((item) => (
+              <div key={item.format}>
+                <Link
+                  href={`https://ckan.data.lnds.lu/dataset/data-available-via-the-luxembourg-microdata-platform-on-labour-and-social-protection.${item.format}`}
+                  className="group flex items-center gap-1"
+                >
+                  <div
+                    className="uppercase text-[14px] font-normal rounded-md px-4 py-1.5 transition"
+                    style={item.style}
+                  >
+                    {item.label}
+                  </div>
+                </Link>
+              </div>
+            ))}
+          </div>
+        </div>
       ),
     },
     {
-      label: "Source",
-      value: createLinkItem({ label: dataset.url, url: dataset.url }),
-    },
-    {
-      label: "Language",
-      value: createLinkItems(
-        dataset.languages?.map((language) => ({
-          label: language.label,
-          url: language.value,
-        }))
+      value: (
+        <div
+          style={{ backgroundColor: "var(--color-surface)" }}
+          className="flex flex-col rounded-2xl p-6 gap-3"
+        >
+          <h1 className="font-bold ">Contact Point</h1>
+          <div className="flex items-center">
+            <div className="h-20 w-20 flex justify-center items-center">
+              {/* Optionally add organization logo here */}
+            </div>
+            <div className="flex flex-col gap-1">
+              <div className="flex gap-2 items-start">
+                <FontAwesomeIcon icon={faUser} size="lg" className="pt-1" />
+                <Link
+                  href={`/datasets?org=${dataset.organization?.title}`}
+                  passHref
+                >
+                  <p className="font-normal">
+                    {dataset.author
+                      ? dataset.author
+                      : dataset.organization
+                        ? dataset.organization.title
+                        : ""}
+                  </p>
+                </Link>
+              </div>
+              <div className="flex gap-2">
+                <FontAwesomeIcon icon={faEnvelope} size="lg" />
+                <p className="font-normal">
+                  {dataset.author_email
+                    ? dataset.author_email
+                    : "No e-mail provided."}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       ),
     },
     {
-      label: "Publisher",
-      value: createTextItem(dataset.publisherName),
-    },
-    {
-      label: "Identifier",
-      value: createTextItem(dataset.identifier),
-    },
-    {
-      label: "Spatial URI",
-      value: createLinkItem({
-        label: dataset.spatial?.label,
-        url: dataset.spatial?.value,
-      }),
-    },
-    {
-      label: "Has Version",
-      value: createLinkItems(
-        dataset.hasVersions?.map((version) => ({
-          label: version.label,
-          url: version.value,
-        }))
+      value: (
+        <div
+          style={{ backgroundColor: "var(--color-surface)" }}
+          className="flex flex-col rounded-2xl p-6 gap-6"
+        >
+          <h1 className="font-extrabold ">Similar Datasets</h1>
+          <div className="flex flex-col gap-8">
+            {dataset.similarDatasets?.slice(0, 2).map((similarDataset) => (
+              <div className="flex flex-col gap-2" key={similarDataset.id}>
+                <Link
+                  href={`/@${similarDataset.organization.title}/${similarDataset.name}`}
+                  className=""
+                >
+                  <h4 className="text-[14px] font-semibold break-words  text-black line-clamp-2">
+                    {similarDataset.title
+                      ? similarDataset.title
+                      : "Untitled Dataset"}
+                  </h4>
+                </Link>
+                <p className=" text-gray line-clamp-3">
+                  {similarDataset.notes
+                    ? similarDataset.notes
+                    : "No description provided."}
+                </p>
+                <div className="flex flex-wrap gap-1 sm:items-center">
+                  <div className="flex flex-wrap gap-1.5">
+                    <FontAwesomeIcon icon={faUser} className="text-accent" />
+                    Published by
+                  </div>
+                  <Link
+                    href={`/datasets?organization=${similarDataset.organization.title}`}
+                  >
+                    <p className="text-gray pl-4 sm:pl-0 line-clamp-2">
+                      {similarDataset.author
+                        ? similarDataset.author
+                        : similarDataset.organization
+                          ? similarDataset.organization.title
+                          : ""}
+                    </p>
+                  </Link>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       ),
-    },
-    {
-      label: "Contact URI",
-      value: createLinkItem({
-        label: dataset.contact?.label,
-        url: dataset.contact?.value,
-      }),
-    },
-    {
-      label: "Access rights",
-      value: createLinkItem({
-        label: dataset.accessRights?.label,
-        url: dataset.accessRights?.value,
-      }),
-    },
-    {
-      label: "Conforms to",
-      value: createLinkItems(
-        dataset.conformsTo?.map((conform) => ({
-          label: conform.label,
-          url: conform.value,
-        }))
-      ),
-    },
-    {
-      label: "Provenance",
-      value: createTextItem(dataset.provenance),
     },
   ];
 }

b080e0da825c3264115333425d8027b0a6c999aa
zia alborzi <zia.alborzi@lnds.lu>
2024-07-30 14:19:13 +0200

    add tooltip

--- src/app/datasets/[id]/DatasetMetadata.tsx
+++ src/app/datasets/[id]/DatasetMetadata.tsx
@@ -14,7 +14,6 @@ import {
   faKey,
   faLanguage,
   faIdBadge,
-  faInfoCircle,
 } from "@fortawesome/free-solid-svg-icons";
 import { formatDate } from "@/utils/formatDate";
 import {
@@ -29,6 +28,7 @@ import {
 } from "@/utils/datasetDictionaryUtils";
 import DistributionAccordion from "./DistributionAccordion";
 import Link from "next/link";
+import Tooltip from "./Tooltip";
 
 const DatasetMetadata = ({
   dataset,
@@ -51,10 +51,7 @@ const DatasetMetadata = ({
             <span className="align-middle">
               Created on {formatDate(dataset.createdAt)}
             </span>
-            <div className="absolute left-0 mt-10 hidden group-hover:flex items-center bg-info text-white text-xs rounded-2xl py-1 px-2 z-10 whitespace-nowrap">
-              <FontAwesomeIcon icon={faInfoCircle} className="mr-1" />
-              Date when the dataset was created.
-            </div>
+            <Tooltip message="Date when the dataset was created." />
           </span>
         )}
         {dataset.createdAt && dataset.modifiedAt && (
@@ -69,10 +66,7 @@ const DatasetMetadata = ({
             <span className="align-middle">
               Modified on {formatDate(dataset.modifiedAt)}
             </span>
-            <div className="absolute left-0 mt-10 hidden group-hover:flex items-center bg-info text-white text-xs rounded-2xl py-1 px-2 z-10 whitespace-nowrap">
-              <FontAwesomeIcon icon={faInfoCircle} className="mr-1" />
-              Date when the dataset was last modified.
-            </div>
+            <Tooltip message="Date when the dataset was last modified." />
           </span>
         )}
         {dataset.catalogue && (
@@ -94,10 +88,7 @@ const DatasetMetadata = ({
                   </h1>
                 </Link>
               </div>
-              <div className="absolute left-0 mt-10 hidden group-hover:flex items-center bg-info text-white text-xs rounded-2xl py-1 px-2 z-10 whitespace-nowrap">
-                <FontAwesomeIcon icon={faInfoCircle} className="mr-1" />
-                Catalogue to which the dataset belongs.
-              </div>
+              <Tooltip message="Catalogue to which the dataset belongs." />
             </span>
           </>
         )}
@@ -112,10 +103,7 @@ const DatasetMetadata = ({
               <span className="align-middle">
                 {dataset.accessRights.label || "No access rights information."}
               </span>
-              <div className="absolute left-0 mt-10 hidden group-hover:flex items-center bg-info text-white text-xs rounded-2xl py-1 px-2 z-10 whitespace-nowrap">
-                <FontAwesomeIcon icon={faInfoCircle} className="mr-1" />
-                Information about access rights to the dataset.
-              </div>
+              <Tooltip message="Information about access rights to the dataset." />
             </span>
           </>
         )}
@@ -130,10 +118,7 @@ const DatasetMetadata = ({
               <span className="align-middle">
                 Identifier: {dataset.identifier}
               </span>
-              <div className="absolute left-0 mt-10 hidden group-hover:flex items-center bg-info text-white text-xs rounded-2xl py-1 px-2 z-10 whitespace-nowrap">
-                <FontAwesomeIcon icon={faInfoCircle} className="mr-1" />
-                Unique identifier for the dataset.
-              </div>
+              <Tooltip message="Unique identifier for the dataset." />
             </span>
           </>
         )}
@@ -149,10 +134,7 @@ const DatasetMetadata = ({
                 Languages:{" "}
                 {dataset.languages?.map((lang) => lang.label).join(", ")}
               </span>
-              <div className="absolute left-0 mt-10 hidden group-hover:flex items-center bg-info text-white text-xs rounded-2xl py-1 px-2 z-10 whitespace-nowrap">
-                <FontAwesomeIcon icon={faInfoCircle} className="mr-1" />
-                Languages in which the dataset is available.
-              </div>
+              <Tooltip message="Languages in which the dataset is available." />
             </span>
           </>
         )}
@@ -167,10 +149,7 @@ const DatasetMetadata = ({
               <span className="align-middle">
                 {dataset.distributions.length} Distribution(s)
               </span>
-              <div className="absolute left-0 mt-10 hidden group-hover:flex items-center bg-info text-white text-xs rounded-2xl py-1 px-2 z-10 whitespace-nowrap">
-                <FontAwesomeIcon icon={faInfoCircle} className="mr-1" />
-                Number of distributions available for the dataset.
-              </div>
+              <Tooltip message="Number of distributions available for the dataset." />
             </span>
           </>
         )}
@@ -186,17 +165,14 @@ const DatasetMetadata = ({
             <div className="flex flex-wrap gap-1">
               {dataset.keywords?.map((keyword) => (
                 <span
-                  className="bg-[var(--color-warning)] bg-opacity-50 px-4 py-1 rounded-2xl text-gray font-[500] text-[14px] inline-block"
+                  className="bg-[var(--color-warning)] bg-opacity-50 px-4 py-1 rounded-2xl-md text-gray font-[500] text-[14px] inline-block"
                   key={keyword.value}
                 >
                   {keyword.label}
                 </span>
               ))}
             </div>
-            <div className="absolute left-0 mt-10 hidden group-hover:flex items-center bg-info text-white text-xs rounded-2xl py-1 px-2 z-10 whitespace-nowrap">
-              <FontAwesomeIcon icon={faInfoCircle} className="mr-1" />
-              Keywords associated with the dataset.
-            </div>
+            <Tooltip message="Keywords associated with the dataset." />
           </span>
         </div>
       )}
@@ -206,7 +182,7 @@ const DatasetMetadata = ({
             {relationships.map((relationship, index) => (
               <div
                 key={index}
-                className="inline-flex bg-[#EFFAFE] px-4 py-1 rounded-2xl text-gray font-[500] text-[14px] group relative"
+                className="inline-flex bg-[#EFFAFE] px-4 py-1 rounded-2xl-md text-gray font-[500] text-[14px] group relative"
               >
                 <Link
                   href={`/@${dataset.catalogue}/${relationship.related_dataset}`}
@@ -221,10 +197,7 @@ const DatasetMetadata = ({
                     {relationship.related_dataset}
                   </span>
                 </Link>
-                <div className="absolute left-0 mt-10 hidden group-hover:flex items-center bg-info text-white text-xs rounded-2xl py-1 px-2 z-10 whitespace-nowrap">
-                  <FontAwesomeIcon icon={faInfoCircle} className="mr-1" />
-                  Related dataset information.
-                </div>
+                <Tooltip message="Related dataset information." />
               </div>
             ))}
           </div>
@@ -254,10 +227,7 @@ const DatasetMetadata = ({
                 <p className="text-sm font-normal text-gray text-[15px] m-0">
                   {formatDescription(entry.description)}
                 </p>
-                <div className="absolute left-0 mt-10 hidden group-hover:flex items-center bg-info text-white text-xs rounded-2xl py-1 px-2 z-10 whitespace-nowrap">
-                  <FontAwesomeIcon icon={faInfoCircle} className="mr-1" />
-                  Information about the field.
-                </div>
+                <Tooltip message="Information about the field." />
               </div>
             ))}
           </div>

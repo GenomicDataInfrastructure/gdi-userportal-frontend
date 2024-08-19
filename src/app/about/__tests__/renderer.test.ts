c9f2246b421229a3f71182dfbca6fc5ce1322695
zia alborzi <zia.alborzi@lnds.lu>
2024-08-19 16:13:04 +0200

    fix linting issue.

--- src/app/about/__tests__/renderer.test.ts
+++ src/app/about/__tests__/renderer.test.ts
@@ -3,9 +3,11 @@
 /* SPDX-License-Identifier: Apache-2.0 */
 
 import renderer from "../renderer";
-import { Tokens } from "marked";
+import { Tokens, RendererObject } from "marked";
 
 describe("Renderer Tests", () => {
+  type RendererContext = RendererObject;
+
   it("should render a heading correctly", () => {
     const token: Tokens.Heading = {
       type: "heading",
@@ -16,7 +18,8 @@ describe("Renderer Tests", () => {
     };
 
     if (renderer.heading) {
-      const result = renderer.heading.call(renderer as any, token);
+      // @ts-expect-error: Using `call` to bind custom `this` context for renderer method
+      const result = renderer.heading.call(renderer as RendererContext, token);
       const expected = `<h1 class="text-left font-medium text-2xl sm:text-3xl mb-4">Heading 1</h1>`;
       expect(result).toBe(expected);
     } else {
@@ -33,7 +36,11 @@ describe("Renderer Tests", () => {
     };
 
     if (renderer.paragraph) {
-      const result = renderer.paragraph.call(renderer as any, token);
+      const result = renderer.paragraph.call(
+        // @ts-expect-error: Using `call` to bind custom `this` context for renderer method
+        renderer as RendererContext,
+        token
+      );
       const expected = `<p class="mb-4">This is a paragraph.</p>`;
       expect(result).toBe(expected);
     } else {
@@ -50,7 +57,7 @@ describe("Renderer Tests", () => {
         task: boolean;
         loose: boolean;
         raw: string;
-        tokens: any[];
+        tokens: unknown[];
         text: string;
         type: string;
       }[];
@@ -79,8 +86,8 @@ describe("Renderer Tests", () => {
     };
 
     if (renderer.list) {
-      // @ts-ignore
-      const result = renderer.list.call(renderer as any, token);
+      // @ts-expect-error: Using `call` to bind custom `this` context for renderer method
+      const result = renderer.list.call(renderer as RendererContext, token);
       const expected = `<ul class="list-inside list-disc mb-4"><li>Item 1</li><li>Item 2</li></ul>`;
       expect(result).toBe(expected);
     } else {
@@ -99,7 +106,8 @@ describe("Renderer Tests", () => {
     };
 
     if (renderer.listitem) {
-      const result = renderer.listitem.call(renderer as any, token);
+      // @ts-expect-error: Using `call` to bind custom `this` context for renderer method
+      const result = renderer.listitem.call(renderer as RendererContext, token);
       const expected = `<li class="mb-2">List item</li>`;
       expect(result).toBe(expected);
     } else {
@@ -117,7 +125,8 @@ describe("Renderer Tests", () => {
     };
 
     if (renderer.link) {
-      const result = renderer.link.call(renderer as any, token);
+      // @ts-expect-error: Using `call` to bind custom `this` context for renderer method
+      const result = renderer.link.call(renderer as RendererContext, token);
       const expected = `<a href="https://example.com" class="text-info hover:underline" target="_blank" rel="noopener noreferrer">Example</a>`;
       expect(result).toBe(expected);
     } else {

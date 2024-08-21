5616159844569ffeab66983a07534d068ca6939b
zia alborzi <zia.alborzi@lnds.lu>
2024-08-21 14:55:07 +0200

    ignore rejectUnauthorized in if explicitly stated in env improve exception handing

--- src/services/discovery/datasetList.ts
+++ src/services/discovery/datasetList.ts
@@ -4,12 +4,25 @@
 
 import { ExtendedSession } from "@/app/api/auth/auth.types";
 import axios, { AxiosResponse } from "axios";
+import https from "https";
 import {
   DatasetSearchOptions,
   DatasetSearchQuery,
   DatasetsSearchResponse,
 } from "./types/datasetSearch.types";
 import { createHeaders } from "./utils";
+
+const axiosInstance = axios.create({
+  baseURL: process.env.API_URL || "",
+});
+
+if (process.env.NODE_IGNORE_SSL) {
+  const httpsAgent = new https.Agent({
+    rejectUnauthorized: false,
+  });
+  axiosInstance.defaults.httpsAgent = httpsAgent;
+  console.log("Development mode: RejectUnauthorized is disabled.");
+}
 
 export const makeDatasetList = (discoveryUrl: string) => {
   return async (
@@ -25,12 +38,23 @@ export const makeDatasetList = (discoveryUrl: string) => {
       operator: options.operator,
     } as DatasetSearchQuery;
 
-    return await axios.post<DatasetsSearchResponse>(
-      `${discoveryUrl}/api/v1/datasets/search`,
-      datasetSearchQuery,
-      {
-        headers: await createHeaders(session),
+    try {
+      return await axiosInstance.post<DatasetsSearchResponse>(
+        `${discoveryUrl}/api/v1/datasets/search`,
+        datasetSearchQuery,
+        {
+          headers: await createHeaders(session),
+        }
+      );
+    } catch (exception) {
+      if (axios.isAxiosError(exception)) {
+        console.log("Error Status:", exception.response?.status);
+        console.log("Error Headers:", exception.response?.headers);
+        console.log("Error Data:", exception.response?.data);
+      } else {
+        console.log("Non-Axios Error:", exception);
       }
-    );
+      throw exception;
+    }
   };
 };

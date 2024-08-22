ff07f1d562a53ff80d46515ff712f6e0481f85d8
Inderpal Singh <inderpal.singh@lnds.lu>
2024-08-22 09:19:19 +0200

    feat: debounce save form to prevent race condition

---
+++ src/utils/__tests__/debounce.test.ts
@@ -0,0 +1,55 @@
+// SPDX-FileCopyrightText: 2024 PNED G.I.E.
+//
+// SPDX-License-Identifier: Apache-2.0
+import debounce from "./../debounce";
+import { jest } from "@jest/globals";
+
+describe("debounce", () => {
+  jest.useFakeTimers();
+
+  it("executes the function after the specified delay", () => {
+    const func = jest.fn();
+    const debouncedFunc = debounce(func, 1000);
+
+    debouncedFunc();
+    expect(func).not.toHaveBeenCalled();
+
+    jest.advanceTimersByTime(500);
+    expect(func).not.toHaveBeenCalled();
+
+    jest.advanceTimersByTime(500);
+    expect(func).toHaveBeenCalled();
+  });
+
+  it("does not execute immediately", () => {
+    const func = jest.fn();
+    const debouncedFunc = debounce(func, 1000);
+
+    debouncedFunc();
+    expect(func).not.toHaveBeenCalled();
+  });
+
+  it("is only executed once within the delay period", () => {
+    const func = jest.fn();
+    const debouncedFunc = debounce(func, 1000);
+
+    debouncedFunc();
+    debouncedFunc();
+    debouncedFunc();
+
+    jest.advanceTimersByTime(1000);
+    expect(func).toHaveBeenCalledTimes(1);
+  });
+
+  it("receives the last provided arguments", () => {
+    const func = jest.fn();
+    const debouncedFunc = debounce(func, 1000);
+
+    debouncedFunc("first call");
+    debouncedFunc("second call");
+    debouncedFunc("last call");
+
+    jest.advanceTimersByTime(1000);
+    expect(func).toHaveBeenCalledWith("last call");
+  });
+});

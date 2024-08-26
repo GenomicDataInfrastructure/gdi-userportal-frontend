cc91b4ad800c23cc0896b8679619b31bed8bf0e1
Kacem Bechka <73106409+nolliia@users.noreply.github.com>
2024-08-26 14:07:18 +0200

    feat: #378 add Skeleton for datasetpage and pagination overhaul (#413)
    * feat: #378 add Skeleton for datasetpage and pagination overhaul
    
    * fix pagination ...
    
    * fix pagination ...
    
    * fix pagination edge cases
    
    ---------
    
    Co-authored-by: Kacem Bechka <kacem.bechka@lnds.lu>

--- src/app/datasets/page.tsx
+++ src/app/datasets/page.tsx
@@ -5,9 +5,7 @@
 "use client";
 
 import Error from "@/app/error";
-import LoadingContainer from "@/components/LoadingContainer";
 import PageContainer from "@/components/PageContainer";
-import PaginationContainer from "@/components/PaginationContainer";
 import SearchBar from "@/components/Searchbar";
 import { datasetList } from "@/services/discovery/index.public";
 import { SearchedDataset } from "@/services/discovery/types/dataset.types";
@@ -21,6 +19,7 @@ import { redirect, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import DatasetList from "../../components/DatasetList";
 import FilterList from "./FilterList";
+import PaginationContainer from "@/components/PaginationContainer";
 
 function parseFacets(queryParams: URLSearchParams): DatasetSearchQueryFacet[] {
   const facetsQuery: DatasetSearchQueryFacet[] = [];
@@ -103,10 +102,38 @@ export default function DatasetPage() {
 
   if (response.status === "loading") {
     return (
-      <LoadingContainer
-        text="Retrieving datasets. This may take a few moments as we process complex Beacon queries."
-        className="mt-4 px-4 text-center sm:mt-8 sm:px-8"
-      />
+      <PageContainer>
+        <div className="grid grid-cols-12 gap-4 p-4">
+          <div className="col-start-0 col-span-12 flex items-center justify-between xl:col-span-10 xl:col-start-2 mb-12">
+            <div className="w-full h-12 bg-gray-200 animate-pulse rounded-lg "></div>
+          </div>
+
+          <div className="col-start-0 col-span-12 flex flex-col gap-4 sm:block xl:hidden">
+            {[...Array(3)].map((_, i) => (
+              <div
+                key={i}
+                className="col-start-0 col-span-12 mt-5 h-12 bg-gray-200 animate-pulse rounded-lg"
+              ></div>
+            ))}
+          </div>
+          <div className="col-start-0 col-span-4 flex flex-col gap-y-6">
+            {[...Array(6)].map((_, i) => (
+              <div
+                key={i}
+                className="col-start-0 col-span-4 mr-6 hidden h-12 bg-gray-200 animate-pulse rounded-lg xl:block px-6"
+              ></div>
+            ))}
+          </div>
+
+          <div className="col-start-0 col-span-12 xl:col-span-8 xl:col-start-5">
+            {[...Array(4)].map((_, i) => (
+              <div key={i} className="w-full mb-4 animate-pulse">
+                <div className="h-36 bg-gray-200 rounded-lg mb-4"></div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </PageContainer>
     );
   } else if (response.status === "error") {
     return <Error statusCode={response.errorCode} />;
@@ -115,56 +142,55 @@ export default function DatasetPage() {
   return (
     <PageContainer>
       <div className="grid grid-cols-12">
-        <>
-          <div className="col-start-0 col-span-12 flex items-center justify-between xl:col-span-10 xl:col-start-2">
-            <SearchBar queryParams={queryParams} />
-          </div>
-          <div className="col-start-0 col-span-12 flex flex-col gap-4 sm:block xl:hidden">
-            {response.facetGroups?.map((group) => {
-              if (group.facets.length > 0) {
-                return (
-                  <div
-                    className="col-start-0 col-span-12 mt-5 h-fit"
-                    key={group.key}
-                  >
-                    <FilterList queryParams={queryParams} facetGroup={group} />
-                  </div>
-                );
-              }
-              return null;
-            })}
-          </div>
-          <p className="col-start-0 col-span-12 mb-12 mt-5 text-center text-sm text-info">
-            {`${response.datasetCount!} ${
-              response.datasetCount! > 1 ? "datasets" : "dataset"
-            } found`}
-          </p>
-          <div className="col-start-0 col-span-4 flex flex-col gap-y-6">
-            {response.facetGroups?.map((group) => {
-              if (group.facets.length > 0) {
-                return (
-                  <div
-                    className=" col-start-0 col-span-4 mr-6 hidden h-fit xl:block px-6"
-                    key={group.key}
-                  >
-                    <FilterList queryParams={queryParams} facetGroup={group} />
-                  </div>
-                );
-              }
-            })}
-          </div>
-          <div className="col-start-0 col-span-12 xl:col-span-8 xl:col-start-5">
-            <DatasetList datasets={response.datasets!} />
-          </div>
-          <div className="col-start-0 col-span-12 mt-10 xl:col-span-8 xl:col-start-5">
-            <PaginationContainer
-              datasetCount={response.datasetCount!}
-              datasetPerPage={DATASET_PER_PAGE}
-              pathname="/datasets"
-              queryParams={queryParams}
-            />
-          </div>
-        </>
+        <div className="col-start-0 col-span-12 flex items-center justify-between xl:col-span-10 xl:col-start-2">
+          <SearchBar queryParams={queryParams} />
+        </div>
+        <div className="col-start-0 col-span-12 flex flex-col gap-4 sm:block xl:hidden">
+          {response.facetGroups?.map((group) => {
+            if (group.facets.length > 0) {
+              return (
+                <div
+                  className="col-start-0 col-span-12 mt-5 h-fit"
+                  key={group.key}
+                >
+                  <FilterList queryParams={queryParams} facetGroup={group} />
+                </div>
+              );
+            }
+            return null;
+          })}
+        </div>
+        <p className="col-start-0 col-span-12 mb-12 mt-5 text-center text-sm text-info">
+          {`${response.datasetCount!} ${
+            response.datasetCount! > 1 ? "datasets" : "dataset"
+          } found`}
+        </p>
+        <div className="col-start-0 col-span-4 flex flex-col gap-y-6">
+          {response.facetGroups?.map((group) => {
+            if (group.facets.length > 0) {
+              return (
+                <div
+                  className="col-start-0 col-span-4 mr-6 hidden h-fit xl:block px-6"
+                  key={group.key}
+                >
+                  <FilterList queryParams={queryParams} facetGroup={group} />
+                </div>
+              );
+            }
+            return null;
+          })}
+        </div>
+        <div className="col-start-0 col-span-12 xl:col-span-8 xl:col-start-5">
+          <DatasetList datasets={response.datasets!} />
+        </div>
+        <div className="col-start-0 col-span-12 my-10 xl:col-span-8 xl:col-start-5">
+          <PaginationContainer
+            datasetCount={response.datasetCount!}
+            datasetPerPage={DATASET_PER_PAGE}
+            pathname="/datasets"
+            queryParams={queryParams}
+          />
+        </div>
       </div>
     </PageContainer>
   );

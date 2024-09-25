8f517f10d3e39f07402bd9b6f4aec6c34483cdf8
zia alborzi <zia.alborzi@lnds.lu>
2024-09-25 09:15:19 +0200

    fix forgotten reuse...

--- src/pages/api/__tests__/sitemap.test.ts
+++ src/pages/api/__tests__/sitemap.test.ts
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2024 PNED G.I.E.
+//
+// SPDX-License-Identifier: Apache-2.0
+
 import { NextApiRequest, NextApiResponse } from "next";
 import { createMocks } from "node-mocks-http";
 import Sitemap from "../sitemap";
--- src/utils/__tests__/fontSelector.test.ts
+++ src/utils/__tests__/fontSelector.test.ts
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2024 PNED G.I.E.
+//
+// SPDX-License-Identifier: Apache-2.0
+
 import { getSelectedFonts, fontVariables } from "../fontSelector";
 
 jest.mock("@/config/theme", () => ({

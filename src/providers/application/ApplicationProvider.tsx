e82ac62e8f4dec9b0981a02679dced7bd618363e
Younès Adem <younes.adem@lnds.lu>
2024-09-27 19:44:49 +0200

    Revert "chore: update delete application to POST"
    This reverts commit 7b73afdbfbfc6363192050dc9297d1289748c58e.

--- src/providers/application/ApplicationProvider.tsx
+++ src/providers/application/ApplicationProvider.tsx
@@ -329,15 +329,12 @@ function ApplicationProvider({ children }: ApplicationProviderProps) {
   async function deleteApplication() {
     dispatch({ type: ApplicationActionType.LOADING });
 
-    const response = await fetch(
-      `/api/applications/${application!.id}/delete`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
+    const response = await fetch(`/api/applications/${application!.id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
 
     if (!response.ok) {
       const errorResponse = await response.json();

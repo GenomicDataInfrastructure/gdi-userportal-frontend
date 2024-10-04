90229e54fdb2096c3a897e8e13f9e6378428a170
Younès Adem <younes.adem@lnds.lu>
2024-10-04 14:56:11 +0200

    chore: minor refactoring

---
+++ src/app/requests/entitlements/EmptyEntitlements.tsx
@@ -0,0 +1,28 @@
+// SPDX-FileCopyrightText: 2024 PNED G.I.E.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+import Button from "@/components/Button";
+import { faPlusCircle } from "@fortawesome/free-solid-svg-icons";
+
+export function EmptyEntitlements() {
+  return (
+    <div className="flex w-full flex-col items-center justify-center gap-4">
+      <p className="text-md text-center text-primary">
+        <span>You don&apos;t have any entitlement yet.</span>
+        <br />
+        <span>
+          Wait for your application(s) to be approved, or submit a new
+          application.
+        </span>
+      </p>
+      <Button
+        icon={faPlusCircle}
+        text="Add datasets"
+        href="/datasets"
+        type="primary"
+        className="text-xs"
+      />
+    </div>
+  );
+}

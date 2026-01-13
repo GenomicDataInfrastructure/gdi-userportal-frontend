7ee01f1c84f88ebed69479729b95c72428722ae9
Rania Hamdani <rania.hamdani@lnds.lu>
2026-01-13 22:06:43 +0100

    fix: add to basket missing dataset params

---
+++ src/app/allele-frequency/components/VariantAddToBasketButton.tsx
@@ -0,0 +1,29 @@
+// SPDX-FileCopyrightText: 2025 Centre for Genomic Regulation
+// SPDX-FileContributor: 2025 PNED G.I.E.
+// SPDX-License-Identifier: Apache-2.0
+"use client";
+
+import React from "react";
+import AddToBasketButton from "@/components/AddToBasketButton";
+import { SearchedDataset } from "@/app/api/discovery/open-api/schemas";
+import { findDatasetByIdentifier } from "@/utils/datasetEntitlements";
+
+type VariantAddToBasketButtonProps = {
+  datasetId: string;
+};
+
+export default function VariantAddToBasketButton({
+  datasetId,
+}: VariantAddToBasketButtonProps) {
+  const [dataset, setDataset] = React.useState<SearchedDataset | null>(null);
+
+  React.useEffect(() => {
+    if (!datasetId) return;
+
+    findDatasetByIdentifier(datasetId)
+      .then((data) => setDataset(data ?? null))
+      .catch(console.error);
+  }, [datasetId]);
+
+  return dataset ? <AddToBasketButton dataset={dataset} /> : null;
+}
